55e77d272c46e6581bfe3b773e702a4213727dc6
exp-3 <takaaki.service@gmail.com>
2016-10-15 16:13:57 +0900

    use for-loop instead of terrible while-loop

--- src/kernel_startup.rs
+++ src/kernel_startup.rs
@@ -4,14 +4,13 @@ use core::intrinsics::volatile_store;
 
 pub fn kernel_startup() {
     extern {
-        static __bss_start: *const u32;
-        static __bss_end: *const u32;
+        static __bss_start: u32;
+        static __bss_end: u32;
     }
-    let mut p = unsafe{__bss_start as *mut u32};
-    while {p as *const u32} < unsafe{__bss_end} {
-        unsafe {
-            volatile_store(p, 0);
-            p = p.offset(1);
+
+    unsafe {
+        for p in __bss_start .. __bss_end {
+                volatile_store(p as *mut u32, 0);
         }
     }
 }
